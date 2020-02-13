deba8fcfa78153e0d69c757fd1d88c532d2be7da
dlib docker <none>
2020-02-13 18:09:08 +0000

    initial commit of the dwm project

---
+++ dwm1001/src/dwm_main_v1.cpp
@@ -0,0 +1,25 @@
+
+// C/C++ includes
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <string>
+#include <vector>
+
+// custom includes
+#include "get_current_time.h"
+#include "get_platform.h"
+#include "num2string.h"
+#include "file_parser.h"
+
+// -------------------------------GLOBALS--------------------------------------
+
+int main(int argc, char** argv)
+{
+
+} // end of main
+
